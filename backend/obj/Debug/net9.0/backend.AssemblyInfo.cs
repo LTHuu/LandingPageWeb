03ed142df94fd5b4b796bb181ec549a7eab634cc
LTHuu <117341204+LTHuu@users.noreply.github.com>
2026-07-04 12:22:46 +0700

    Ignore build artifacts

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b683ee9bac2d3a4d9532c545ef40c684a7f75ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7fa545301caea68761648ca1337ae7d4fd9f2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
